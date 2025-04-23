901180eaf953f05b27844c42d65cefba7d1618cb
Joshua <dixonjo22@students.ecu.edu>
2025-04-23 14:11:01 -0400

    i forgot to change db to the docker one...

--- docker-webapp/assignment/Webpages/csdashboard.py
+++ docker-webapp/assignment/Webpages/csdashboard.py
@@ -3,7 +3,7 @@ import psycopg2
 import psycopg2.extras
 
 # Database connection
-conn = psycopg2.connect("host=192.168.56.30 dbname=dashboard user=webuser1 password=student")
+conn = psycopg2.connect("host=localhost dbname=webapp_db user=postgres password=student")
 cursor = conn.cursor()
 
 print("Content-type: text/html\n\n")
